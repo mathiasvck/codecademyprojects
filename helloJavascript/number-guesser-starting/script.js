f3752767503c309b791ff411af8a90e1264c794b
Mathias <71843132+mathiasvck@users.noreply.github.com>
2025-01-14 13:18:16 +0100

    adapt style and script

--- helloJavascript/number-guesser-starting/script.js
+++ helloJavascript/number-guesser-starting/script.js
@@ -7,4 +7,33 @@ function generateTarget(){
     return Math.floor(Math.random()*9);
 };
 
-console.log(generateTarget());
+//console.log(generateTarget());
+
+function compareGuesses(userGuess, computerGuess, target){
+    const diff1 = Math.abs(userGuess- target);
+    const diff2 = Math.abs(computerGuess- target);
+
+    if (diff1 < diff2 ){
+        return true;
+    } else if (diff2 < diff1){
+        return false;
+    } else {
+        return true;
+    };
+};
+
+/*console.log(compareGuesses(2,3,1));
+console.log(compareGuesses(2,3,4));
+console.log(compareGuesses(2,2,1));*/
+
+function updateScore(winner){
+    if (winner === "human"){
+        humanScore ++;
+    } else if (winner === "computer"){
+        computerScore ++;
+    };
+};
+
+function advanceRound(){
+    currentRoundNumber ++;
+};
